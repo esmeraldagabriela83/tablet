d395301a8874e65d3009cc3f8e11d0ff8f27ff22
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2023-04-08 19:58:38 +0300

    school form email

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,21 +1,27 @@
 console.log("hello from contact js");
 
+const sentForm=document.getElementById("sentForm");
+
+const resetForm=document.getElementById("resetForm");
+
 //----
 
 const name=document.getElementById("name");
 console.log("name is : " , name);
 
-const contact =document.getElementById("email");
-console.log("email is : " , email);
+const mail =document.getElementById("mail");
+console.log("mail is : " , mail);
 
 
-const message=document.getElementById("message");
-console.log("message is " , message);
+const comment=document.getElementById("comment");
+console.log("comment is " , comment);
 
 
 const submit=document.getElementById("submit");
 console.log("submit is " , submit);
 
+const reset=document.getElementById("reset");
+console.log("reset" ,reset);
 
 
 submit.addEventListener("click" , function(event){
@@ -27,8 +33,21 @@ submit.addEventListener("click" , function(event){
     //----
 console.log("name value is " , name.value);
 
-console.log("email value is " , email.value);
+console.log("mail value is " , mail.value);
 
-console.log("message value is " , message.value);
+console.log("comment value is " , comment.value);
     //----
+
+    sentForm.innerText="sent form";
+
+});
+
+//----
+
+reset.addEventListener("click" , function(event){
+    event.preventDefault();
+
+    console.log("you clicked the reset btn");
+
+    resetForm.innerText="reset form";
 });
